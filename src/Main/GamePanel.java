439aa5c03db0a7677ff31e6d168ca078def37ed7
icyfillup <phil10tran@gmail.com>
2014-06-26 00:07:34 -0700

    Added Resource file and created a package called Main with the class file Game and GamePanel

---
+++ src/Main/GamePanel.java
@@ -0,0 +1,116 @@
+package Main;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements KeyListener, Runnable
+{
+//	dimenions
+	public static final int WIDTH = 320;
+	public static final int HEIGHT = 240;
+	public static final int SCALE = 2;
+	
+//	Game Thread
+	private Thread thread;
+	private boolean running;
+	private int FPS = 60;
+	private long targetTime = 1000 / FPS;
+	
+//	Image
+	private BufferedImage image;
+	private Graphics2D g;
+	
+	public GamePanel()
+	{
+		super();
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setFocusable(true);
+		requestFocus();
+	}
+
+	public void addNotify()
+	{
+		super.addNotify();
+		if(thread == null)
+		{ 
+			thread = new Thread(this); 
+			addKeyListener(this);
+			thread.start();
+		}
+	}
+	
+	public void run() 
+	{
+		init();
+		
+		long start;
+		long elapsed;
+		long wait;
+		
+//		Game Loop
+		while(running)
+		{
+			start = System.nanoTime();
+			
+			update();
+			draw();
+			drawToScreen();
+			
+			elapsed = System.nanoTime() - start;
+			
+			wait = targetTime - elapsed / 1000000;
+			
+			try
+			{
+				Thread.sleep(wait);	
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void update()
+	{
+		
+	}
+	
+	private void draw() 
+	{
+		
+	}
+	
+	private void drawToScreen()
+	{
+		Graphics g2 = getGraphics();
+		g2.drawImage(image, 0, 0, null);
+		g2.dispose();
+	}
+	
+	private void init()
+	{
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		g = (Graphics2D) g;
+		
+		running = true;
+	}
+	
+	public void keyTyped(KeyEvent key)
+	{
+		
+	}
+
+	public void keyPressed(KeyEvent key)
+	{
+		
+	}
+
+	public void keyReleased(KeyEvent key)
+	{
+		
+	}
+}
